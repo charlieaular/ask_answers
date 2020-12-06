e1046eb48f57ad69fc8d4bbfba6f7eb100a4b778
charlie <charlieaular@gmail.com>
2020-12-05 19:19:15 -0500

    previniendo scrennshots

--- android/app/src/main/kotlin/com/example/ask_answers/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ask_answers/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.ask_answers
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Bundle
 import android.os.Build
 import android.view.ViewTreeObserver
 import android.view.WindowManager
+
+import android.view.WindowManager.LayoutParams;
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(LayoutParams.FLAG_SECURE);
+    }
 }
